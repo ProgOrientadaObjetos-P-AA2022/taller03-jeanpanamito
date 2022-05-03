217a96a1d1f2a2cd789b3b986cfc371f15845c98
jeanpanamito <jppanamito@utpl.edu.ec>
2022-05-03 12:21:32 -0500

    cambios

--- Ejemplo/ManejoClasesObjetos/src/paquetedos/ejecutor.java
+++ Ejemplo/ManejoClasesObjetos/src/paquetedos/ejecutor.java
@@ -3,20 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paqueteuno;
+package paquetedos;
+
+import paqueteuno.FacturaTelefono;
 
 /**
  *
- * @author reroes
+ * @author PERSONAL
  */
-public class Ejecutor {
-    public static void main(String[] args) {
+public class ejecutor {
+     public static void main(String[] args) {
         // Crear un objeto de tipo FacturaTelefono
-        FacturaTelefono ft = new FacturaTelefono();
+       
+         FacturaTelefono ft = new FacturaTelefono(); 
+         FacturaTelefono ft2 = new FacturaTelefono();
         // Valores de entrada
         String numeroTelefono = "098321321";
-        double minutos = 230;
-        double valorMinuto = 0.25;
+        double minutos = 30;
+        double valorMinuto = 0.15;
+        String numeroTelefono1 = "09832132234";
+        double minutos1 = 31;
+        double valorMinuto1 = 0.15;
+        ft2.establecerNumeroTelefono(numeroTelefono);
+        ft2.establecerMinutosMes(minutos);
+        ft2.establecerValorMinuto(valorMinuto);
         // uso de los métodos establecer para darle valores a los 
         // atributos del objeto.
         ft.establecerNumeroTelefono(numeroTelefono);
@@ -39,5 +49,11 @@ public class Ejecutor {
                 + "Valor a cancelar: %.2f\n", ft.obtenerNumeroTelefono(),
                 ft.obtenerMinutosMes(), ft.obtenerValorMinuto(),
                 ft.obtenerValorFactura());
-    }
+        System.out.printf("\nFactura de Teléfono\n\nNúmero de teléfono: %s\n"
+                + "Minutos mensuales: %.2f\nValor por minuto: %.2f\n"
+                + "Valor a cancelar: %.2f\n", ft.obtenerNumeroTelefono(),
+                ft.obtenerMinutosMes(), ft.obtenerValorMinuto(),
+                ft.obtenerValorFactura());
+    }  
+     
 }
